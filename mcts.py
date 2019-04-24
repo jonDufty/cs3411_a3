487fcc7c3802f5a4d9e82a062ae424762c3c7392
BeastlyBrosif <nimrodwynne@gmail.com>
2019-04-24 12:28:23 +1000

    work on mcts

--- mcts.py
+++ mcts.py
@@ -1,3 +1,5 @@
+#MCTS implementation by Jon Duffy and Nimrod Wynne
+
 class MonteCarlo:
     def __init__(self):
         pass
@@ -6,7 +8,7 @@ class MonteCarlo:
     def select_node(self):
         pass
 
-    # Expands selected node to find potential moves 
+    # Expands selected node to find potential moves
     def expand_node(self):
         pass
 
@@ -21,3 +23,32 @@ class MonteCarlo:
         self.run_simulation()
         # Pick node with best statistic
         return move
+
+#these are nodes, that will make up the tree
+class Board(object):
+    def start(self):
+        # Returns a representation of the starting state of the game.
+        pass
+
+    def current_player(self, state):
+        # Takes the game state and returns the current player's
+        # number.
+        pass
+
+    def next_state(self, state, play):
+        # Takes the game state, and the move to be applied.
+        # Returns the new game state.
+        pass
+
+    def legal_plays(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history, and returns the full list of moves that
+        # are legal plays for the current player.
+        pass
+
+    def winner(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history.  If the game is now won, return the player
+        # number.  If the game is still ongoing, return zero.  If
+        # the game is tied, return a different distinct value, e.g. -1.
+        pass
